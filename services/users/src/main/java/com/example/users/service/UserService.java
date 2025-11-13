ee2bb5b47fd9d2bdc7e5156c11b761a2e1f9af79
NgHVu <nhoangvu2306@gmail.com>
2025-11-13 19:23:42 +0700

    Thêm loginWithGoogle

--- services/users/src/main/java/com/example/users/service/UserService.java
+++ services/users/src/main/java/com/example/users/service/UserService.java
@@ -9,37 +9,17 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface UserService extends UserDetailsService {
 
-    /**
-     * @param registerRequest DTO chứa thông tin đăng ký.
-     */
     void registerUser(RegisterRequest registerRequest);
 
-    /**
-     * @param verifyRequest DTO chứa email và OTP.
-     * @return DTO chứa access token.
-     */
     AuthResponse verifyAccount(VerifyRequest verifyRequest);
 
-    /**    
-     * @param loginRequest DTO chứa thông tin đăng nhập.
-     * @return DTO chứa access token.
-     */
     AuthResponse loginUser(LoginRequest loginRequest);
 
-    /**
-     * Lấy thông tin người dùng hiện tại (đã xác thực).
-     *
-     * @return DTO chứa thông tin công khai của người dùng hiện tại.
-     */
     UserResponse getCurrentUser();
 
-    /**
-     * Lấy thông tin người dùng bằng email (dùng cho nội bộ service).
-     *
-     * @param email Email cần tìm.
-     * @return DTO chứa thông tin công khai của người dùng.
-     */
     UserResponse findUserByEmail(String email);
 
     void resendOtp(String email);
+
+    AuthResponse loginWithGoogle(String authorizationCode);
 }
