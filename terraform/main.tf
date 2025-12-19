63a995b6adb9cc2ffbfaa63556b954f9dd322704
NgHVu <nhoangvu2306@gmail.com>
2025-12-19 17:58:11 +0700

    Apply terraform và update jenkins (dev)

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "jenkins_server" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
-  
+
   vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
   iam_instance_profile   = aws_iam_instance_profile.jenkins_profile.name
 
