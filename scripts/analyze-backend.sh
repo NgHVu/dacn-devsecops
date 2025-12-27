d4964af7398f8e325b36599e183030fb77092b12
NgHVu <nhoangvu2306@gmail.com>
2025-12-27 22:01:29 +0700

    Update Jenkinsfile

--- scripts/analyze-backend.sh
+++ scripts/analyze-backend.sh
@@ -2,22 +2,16 @@
 set -e # Dừng ngay nếu có lệnh bị lỗi
 
 SERVICE_NAME=$1
-# Đặt tên Project Key thống nhất
-SONAR_PROJECT_KEY="foodhub-${SERVICE_NAME}"
+SONAR_PROJECT_KEY="${SERVICE_NAME}-service"
 
 echo "--> [BACKEND] Bắt đầu Test & Phân tích cho: $SERVICE_NAME"
 
 # Chuyển vào thư mục service
 cd "services/$SERVICE_NAME"
 
-# 1. Sửa lỗi "No plugin found": Dùng tên đầy đủ (Fully Qualified Name) của plugin.
-# 2. Sửa lỗi Auth: Truyền biến môi trường SONAR_HOST_URL và SONAR_AUTH_TOKEN từ Jenkins vào.
-# 3. Skip lỗi test: Để Sonar vẫn báo cáo kết quả quét code dù Unit Test có fail.
-
-mvn clean verify org.sonarsource.scanner.maven:sonar-maven-plugin:3.11.0.3922:sonar \
+# Chạy Maven Verify & SonarQube
+mvn clean verify sonar:sonar \
     -Dsonar.projectKey=$SONAR_PROJECT_KEY \
-    -Dsonar.host.url=$SONAR_HOST_URL \
-    -Dsonar.login=$SONAR_AUTH_TOKEN \
-    -Dmaven.test.failure.ignore=true
+    -Dmaven.test.failure.ignore=false
 
 echo "--> [BACKEND] Hoàn tất phân tích Code Quality."
